a6115736c49de5e697eb5b1ec6e51eaeecc47426
Kodux78k <koduxlive@gmail.com>
2025-11-06 05:28:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='lv-offline-v1';
+const CORE=['./','./index.html','./Livro-vivo-93beatyv3-PWA.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(e.request.method!=='GET'||url.origin!==self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(hit=>hit||fetch(e.request).then(resp=>{const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return resp;}).catch(()=>caches.match('./Livro-vivo-93beatyv3-PWA.html')))
+  );
+});
